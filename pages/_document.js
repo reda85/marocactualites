f618faf5e883d1e44f6de6ebe97f879c218e7f61
Marouane Reda <redamarouane@gmail.com>
2023-04-06 00:22:11 +0000

    bfbf

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,20 @@ export default class MyDocument extends Document {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
 
-
+        <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_TRACKING_ID}');
+                    `,
+                  }}
+                />
  
         <link
               href="https://fonts.googleapis.com/css2?family=Rubik&display=optionnal"
